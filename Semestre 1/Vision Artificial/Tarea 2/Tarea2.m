ea7aafc01baf1621e22b2d6f5ef065ad4fa6e802
Cesar Torres <catq2596@gmail.com>
2021-02-24 02:02:40 -0800

    Correction in artf vision hmwrk

--- Semestre 1/Vision Artificial/Tarea 2/Tarea2.m
+++ Semestre 1/Vision Artificial/Tarea 2/Tarea2.m
@@ -4,11 +4,19 @@ img2 = imread('../imgs/color/flower.jpg');
 img3 = imread('../imgs/color/racoon.png');
 img4 = imread('../imgs/color/strawberries_coffee_full_color.png');
 % Calculate shape of image
+
 "Shape of img1 (Hedgehog)"
 img_size = size(img);
 height = img_size(1)
 width = img_size(2)
 dim = img_size(3)
+ttle = ['height:' num2str(height) ', width:' num2str(width) ', dim:' num2str(dim)];
+%Display image
+figure("Name","Tarea 2 Figura 1")
+subplot(1,1,1); 
+imshow(img);
+title(ttle);
+
 %Create some variations of the image
 windowSize = 15;
 avg3 = ones(windowSize) / windowSize^2;
@@ -17,7 +25,7 @@ rotated_img = imrotate(img3,180); % 180 Degree rotation in image
 grayscale = rgb2gray(img4); % Convert image to Grayscale
 %%% DISPLAY IMAGES
 % Create figure
-figure("Name","Tarea 2")
+figure("Name","Tarea 2 Figura 2")
 subplot(2,2,1); 
 imshow(img);
 title("Original Image");
