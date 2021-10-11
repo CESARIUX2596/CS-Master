ddfac116b206e047502ada1344091de19616ee4e
Cesar Torres <catq2596@gmail.com>
2021-10-10 18:58:56 -0700

    Formulas Ch5 and Ch6 Statistics

--- Semestre 2/Redes Neuronales/Simple Neural Network Example/neuralNetworkFF.m
+++ Semestre 2/Redes Neuronales/Simple Neural Network Example/neuralNetworkFF.m
@@ -1,17 +1,22 @@
 % https://la.mathworks.com/help/deeplearning/ref/feedforwardnet.html?s_tid=doc_ta
 
-x = -1:0.05:1;
-y = -0.5*x.^3+1*cos(x*5)+exp(x)-2;
+x = [-1:0.05:1];
+y = [-0.5*x.^3+1*cos(x*5)+exp(x)-2];
 
 figure; hold on;
 plot(x,y,'x');
 
+% define ammount of neurons in hidden layers
 netconf = [10];
-net = feedforwardnet(netconf);
+
+net = feedforwardnet(netconf,'trainlm');
+options  = trainingOptions('sgdm', ...
+        'MaxEpochs', 500);
+
+net = trainNetwork(x,y,options);
 % net = trainlm(net,x,y);  % Levenberg Marquardt
 % net = traingd(net,x,y);  % Gradient Descent
-net = traingdm(net,x,y); % Gradient Descent Momentum
-
+% net = traingdm(net,x,y); % Gradient Descent Momentum
 y_pred = net(x);
 plot(x,y_pred,'linewidth',0.5);
 perf = perform(net,y_pred,y)
